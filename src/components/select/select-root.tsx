dc726aefcf0d458314f35e33c057c26718a46652
Rasmus Madsen <76999316+imMadsen@users.noreply.github.com>
2024-09-10 11:13:12 +0200

    Fixed Select Component
    Missing
    selectItemComponent
    selectItemLabelComponent

--- src/components/select/select-root.tsx
+++ src/components/select/select-root.tsx
@@ -1,3 +1,12 @@
+import { PropsOf } from "@builder.io/qwik";
 import { Select as HeadlessSelect } from "@qwik-ui/headless";
+import { SelectItem } from "./select-item";
+import { SelectItemLabel } from "./select-item-label";
 
-export const SelectRoot: typeof HeadlessSelect.Root = HeadlessSelect.Root;
+export const SelectRoot = (props: PropsOf<typeof HeadlessSelect.Root>) => (
+    <HeadlessSelect.Root
+      {...props}
+      selectItemComponent={SelectItem}
+      selectItemLabelComponent={SelectItemLabel}
+    />
+  );
