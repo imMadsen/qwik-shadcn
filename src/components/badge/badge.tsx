8663b953c024a2865edc3abb782c1734b212415c
Rasmus Madsen <76999316+imMadsen@users.noreply.github.com>
2024-12-11 17:27:21 +0100

    Update badge.tsx

--- src/components/badge/badge.tsx
+++ src/components/badge/badge.tsx
@@ -4,7 +4,7 @@ import { component$, Slot, type QwikJSX } from "@builder.io/qwik"
 import { cn } from "@qwik-ui/utils"
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
   {
     variants: {
       variant: {
